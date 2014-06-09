5cd02a7b1f3534537138bc236b89dc837c9acd6f
douwevdijk <douwevdijk@hotmail.com>
2014-06-09 18:50:11 +0200

    ncsah template

---
+++ contacts.php
@@ -0,0 +1,32 @@
+<?php
+
+require("phpmail/class.phpmailer.php");
+
+$mail = new PHPMailer(); 
+
+$mail->From = "douwe@dropandreport.com";
+$mail->AddReplyTo($_POST["email"], $_POST["name"]);
+
+$mail->FromName = addslashes(utf8_decode($_POST["name"]));
+$mail->AddAddress("douwevdijk@hotmail.com");  
+ 
+$mail->Subject  = "E-mail from website";
+$mail->Body     = "From: ". $_POST["name"] . "\r\n";
+$mail->Body     .= "Email: ". $_POST["email"] . "\r\n";
+$mail->Body     .= "Message: ". addslashes(utf8_decode($_POST["message"]));
+ 
+if(!$mail->Send()) {
+$return['success'] = false;
+}
+
+	else
+{
+$return['success'] = true;
+}
+
+echo json_encode($return);
+
+
+?>
+
+	
